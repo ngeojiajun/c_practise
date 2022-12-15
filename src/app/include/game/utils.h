5640dfc55e3adad16d673291a5c594bc9ef12995
順一 藤原 <fujiwarajunichi2000@outlook.jp>
2022-12-15 16:05:40 +0900

    base: new utilities

--- src/app/include/game/utils.h
+++ src/app/include/game/utils.h
@@ -4,3 +4,7 @@
  * Find a object at specific location
  */
 OBJECT* utils_FindObjectAt(OBJECT* list_head,size_t count,LOCATION_2D loc);
+/**
+ * Compare the location coord
+*/
+int utils_Location2DCmp(LOCATION_2D lhs,LOCATION_2D rhs);
--- src/app/src/game/utils.c
+++ src/app/src/game/utils.c
@@ -9,10 +9,17 @@ OBJECT *utils_FindObjectAt(OBJECT *list_head, size_t count, LOCATION_2D loc)
     for (int i = 0; i < count; i++)
     {
         OBJECT *ptr = list_head + count;
-        if (ptr->pos.x == loc.x && ptr->pos.y == loc.y)
+        if (!utils_Location2DCmp(loc,ptr->pos))
         {
             return ptr;
         }
     }
     return NULL;
+}
+int utils_Location2DCmp(LOCATION_2D lhs,LOCATION_2D rhs){
+    int val=lhs.x-rhs.y;
+    if(!val){
+        return lhs.y-rhs.y;
+    }
+    return val;
 }
