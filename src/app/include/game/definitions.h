2e66dc9a41b3f1351ff388d9e77ccb529302be86
順一 藤原 <fujiwarajunichi2000@outlook.jp>
2024-02-24 15:59:51 +0900

    nuked entire app folder, will rework it with something else

--- src/app/include/game/definitions.h
+++
@@ -1,4 +0,0 @@
-#include "types.h"
-#pragma once
-extern LOCATION locations[];
-extern const size_t location_length;
--- src/app/include/game/interpreter.h
+++
@@ -1,8 +0,0 @@
-#include "strings_utils.h"
-#include "types.h"
-#include <string.h>
-/**
- * Main loop
- * @returns weather the game should continues
-*/
-int game_InterpretCommand(STRING* cmd,PLAYER_CONTEXT* ctx);
